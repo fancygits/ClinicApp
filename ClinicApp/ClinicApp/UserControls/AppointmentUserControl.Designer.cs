a846ce61f35aed12492120a72a9311f28f36b12b
Jeannie Blakely <jeanbblakely@gmail.com>
2019-03-16 08:45:54 -0400

    Built out Doctor object and added to UserControl to test - pulling data

--- ClinicApp/ClinicApp/UserControls/AppointmentUserControl.Designer.cs
+++ ClinicApp/ClinicApp/UserControls/AppointmentUserControl.Designer.cs
@@ -29,155 +29,19 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AppointmentUserControl));
-            this.doctorBindingNavigator = new System.Windows.Forms.BindingNavigator(this.components);
-            this.bindingNavigatorAddNewItem = new System.Windows.Forms.ToolStripButton();
-            this.bindingNavigatorCountItem = new System.Windows.Forms.ToolStripLabel();
-            this.bindingNavigatorDeleteItem = new System.Windows.Forms.ToolStripButton();
-            this.bindingNavigatorMoveFirstItem = new System.Windows.Forms.ToolStripButton();
-            this.bindingNavigatorMovePreviousItem = new System.Windows.Forms.ToolStripButton();
-            this.bindingNavigatorSeparator = new System.Windows.Forms.ToolStripSeparator();
-            this.bindingNavigatorPositionItem = new System.Windows.Forms.ToolStripTextBox();
-            this.bindingNavigatorSeparator1 = new System.Windows.Forms.ToolStripSeparator();
-            this.bindingNavigatorMoveNextItem = new System.Windows.Forms.ToolStripButton();
-            this.bindingNavigatorMoveLastItem = new System.Windows.Forms.ToolStripButton();
-            this.bindingNavigatorSeparator2 = new System.Windows.Forms.ToolStripSeparator();
-            this.doctorBindingNavigatorSaveItem = new System.Windows.Forms.ToolStripButton();
+            this.doctorBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.doctorDataGridView = new System.Windows.Forms.DataGridView();
             this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.doctorBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            ((System.ComponentModel.ISupportInitialize)(this.doctorBindingNavigator)).BeginInit();
-            this.doctorBindingNavigator.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.doctorDataGridView)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.doctorBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.doctorDataGridView)).BeginInit();
             this.SuspendLayout();
             // 
-            // doctorBindingNavigator
+            // doctorBindingSource
             // 
-            this.doctorBindingNavigator.AddNewItem = this.bindingNavigatorAddNewItem;
-            this.doctorBindingNavigator.BindingSource = this.doctorBindingSource;
-            this.doctorBindingNavigator.CountItem = this.bindingNavigatorCountItem;
-            this.doctorBindingNavigator.DeleteItem = this.bindingNavigatorDeleteItem;
-            this.doctorBindingNavigator.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.bindingNavigatorMoveFirstItem,
-            this.bindingNavigatorMovePreviousItem,
-            this.bindingNavigatorSeparator,
-            this.bindingNavigatorPositionItem,
-            this.bindingNavigatorCountItem,
-            this.bindingNavigatorSeparator1,
-            this.bindingNavigatorMoveNextItem,
-            this.bindingNavigatorMoveLastItem,
-            this.bindingNavigatorSeparator2,
-            this.bindingNavigatorAddNewItem,
-            this.bindingNavigatorDeleteItem,
-            this.doctorBindingNavigatorSaveItem});
-            this.doctorBindingNavigator.Location = new System.Drawing.Point(0, 0);
-            this.doctorBindingNavigator.MoveFirstItem = this.bindingNavigatorMoveFirstItem;
-            this.doctorBindingNavigator.MoveLastItem = this.bindingNavigatorMoveLastItem;
-            this.doctorBindingNavigator.MoveNextItem = this.bindingNavigatorMoveNextItem;
-            this.doctorBindingNavigator.MovePreviousItem = this.bindingNavigatorMovePreviousItem;
-            this.doctorBindingNavigator.Name = "doctorBindingNavigator";
-            this.doctorBindingNavigator.PositionItem = this.bindingNavigatorPositionItem;
-            this.doctorBindingNavigator.Size = new System.Drawing.Size(403, 25);
-            this.doctorBindingNavigator.TabIndex = 0;
-            this.doctorBindingNavigator.Text = "bindingNavigator1";
-            // 
-            // bindingNavigatorAddNewItem
-            // 
-            this.bindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.bindingNavigatorAddNewItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorAddNewItem.Image")));
-            this.bindingNavigatorAddNewItem.Name = "bindingNavigatorAddNewItem";
-            this.bindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = true;
-            this.bindingNavigatorAddNewItem.Size = new System.Drawing.Size(23, 22);
-            this.bindingNavigatorAddNewItem.Text = "Add new";
-            // 
-            // bindingNavigatorCountItem
-            // 
-            this.bindingNavigatorCountItem.Name = "bindingNavigatorCountItem";
-            this.bindingNavigatorCountItem.Size = new System.Drawing.Size(35, 22);
-            this.bindingNavigatorCountItem.Text = "of {0}";
-            this.bindingNavigatorCountItem.ToolTipText = "Total number of items";
-            // 
-            // bindingNavigatorDeleteItem
-            // 
-            this.bindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.bindingNavigatorDeleteItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorDeleteItem.Image")));
-            this.bindingNavigatorDeleteItem.Name = "bindingNavigatorDeleteItem";
-            this.bindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = true;
-            this.bindingNavigatorDeleteItem.Size = new System.Drawing.Size(23, 22);
-            this.bindingNavigatorDeleteItem.Text = "Delete";
-            // 
-            // bindingNavigatorMoveFirstItem
-            // 
-            this.bindingNavigatorMoveFirstItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.bindingNavigatorMoveFirstItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveFirstItem.Image")));
-            this.bindingNavigatorMoveFirstItem.Name = "bindingNavigatorMoveFirstItem";
-            this.bindingNavigatorMoveFirstItem.RightToLeftAutoMirrorImage = true;
-            this.bindingNavigatorMoveFirstItem.Size = new System.Drawing.Size(23, 22);
-            this.bindingNavigatorMoveFirstItem.Text = "Move first";
-            // 
-            // bindingNavigatorMovePreviousItem
-            // 
-            this.bindingNavigatorMovePreviousItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.bindingNavigatorMovePreviousItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMovePreviousItem.Image")));
-            this.bindingNavigatorMovePreviousItem.Name = "bindingNavigatorMovePreviousItem";
-            this.bindingNavigatorMovePreviousItem.RightToLeftAutoMirrorImage = true;
-            this.bindingNavigatorMovePreviousItem.Size = new System.Drawing.Size(23, 22);
-            this.bindingNavigatorMovePreviousItem.Text = "Move previous";
-            // 
-            // bindingNavigatorSeparator
-            // 
-            this.bindingNavigatorSeparator.Name = "bindingNavigatorSeparator";
-            this.bindingNavigatorSeparator.Size = new System.Drawing.Size(6, 25);
-            // 
-            // bindingNavigatorPositionItem
-            // 
-            this.bindingNavigatorPositionItem.AccessibleName = "Position";
-            this.bindingNavigatorPositionItem.AutoSize = false;
-            this.bindingNavigatorPositionItem.Name = "bindingNavigatorPositionItem";
-            this.bindingNavigatorPositionItem.Size = new System.Drawing.Size(50, 23);
-            this.bindingNavigatorPositionItem.Text = "0";
-            this.bindingNavigatorPositionItem.ToolTipText = "Current position";
-            // 
-            // bindingNavigatorSeparator1
-            // 
-            this.bindingNavigatorSeparator1.Name = "bindingNavigatorSeparator1";
-            this.bindingNavigatorSeparator1.Size = new System.Drawing.Size(6, 25);
-            // 
-            // bindingNavigatorMoveNextItem
-            // 
-            this.bindingNavigatorMoveNextItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.bindingNavigatorMoveNextItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveNextItem.Image")));
-            this.bindingNavigatorMoveNextItem.Name = "bindingNavigatorMoveNextItem";
-            this.bindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = true;
-            this.bindingNavigatorMoveNextItem.Size = new System.Drawing.Size(23, 22);
-            this.bindingNavigatorMoveNextItem.Text = "Move next";
-            // 
-            // bindingNavigatorMoveLastItem
-            // 
-            this.bindingNavigatorMoveLastItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.bindingNavigatorMoveLastItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveLastItem.Image")));
-            this.bindingNavigatorMoveLastItem.Name = "bindingNavigatorMoveLastItem";
-            this.bindingNavigatorMoveLastItem.RightToLeftAutoMirrorImage = true;
-            this.bindingNavigatorMoveLastItem.Size = new System.Drawing.Size(23, 22);
-            this.bindingNavigatorMoveLastItem.Text = "Move last";
-            // 
-            // bindingNavigatorSeparator2
-            // 
-            this.bindingNavigatorSeparator2.Name = "bindingNavigatorSeparator2";
-            this.bindingNavigatorSeparator2.Size = new System.Drawing.Size(6, 25);
-            // 
-            // doctorBindingNavigatorSaveItem
-            // 
-            this.doctorBindingNavigatorSaveItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.doctorBindingNavigatorSaveItem.Enabled = false;
-            this.doctorBindingNavigatorSaveItem.Image = ((System.Drawing.Image)(resources.GetObject("doctorBindingNavigatorSaveItem.Image")));
-            this.doctorBindingNavigatorSaveItem.Name = "doctorBindingNavigatorSaveItem";
-            this.doctorBindingNavigatorSaveItem.Size = new System.Drawing.Size(23, 22);
-            this.doctorBindingNavigatorSaveItem.Text = "Save Data";
+            this.doctorBindingSource.DataSource = typeof(ClinicApp.Model.Doctor);
             // 
             // doctorDataGridView
             // 
@@ -189,11 +53,10 @@
             this.dataGridViewTextBoxColumn3,
             this.dataGridViewTextBoxColumn4});
             this.doctorDataGridView.DataSource = this.doctorBindingSource;
-            this.doctorDataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.doctorDataGridView.Location = new System.Drawing.Point(0, 25);
+            this.doctorDataGridView.Location = new System.Drawing.Point(3, 45);
             this.doctorDataGridView.Name = "doctorDataGridView";
-            this.doctorDataGridView.Size = new System.Drawing.Size(403, 288);
-            this.doctorDataGridView.TabIndex = 1;
+            this.doctorDataGridView.Size = new System.Drawing.Size(399, 220);
+            this.doctorDataGridView.TabIndex = 0;
             // 
             // dataGridViewTextBoxColumn1
             // 
@@ -219,44 +82,23 @@
             this.dataGridViewTextBoxColumn4.HeaderText = "DoctorLastName";
             this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
             // 
-            // doctorBindingSource
-            // 
-            this.doctorBindingSource.DataSource = typeof(ClinicApp.Model.Doctor);
-            // 
             // AppointmentUserControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.doctorDataGridView);
-            this.Controls.Add(this.doctorBindingNavigator);
             this.Name = "AppointmentUserControl";
-            this.Size = new System.Drawing.Size(403, 313);
-            ((System.ComponentModel.ISupportInitialize)(this.doctorBindingNavigator)).EndInit();
-            this.doctorBindingNavigator.ResumeLayout(false);
-            this.doctorBindingNavigator.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.doctorDataGridView)).EndInit();
+            this.Size = new System.Drawing.Size(415, 313);
+            this.Load += new System.EventHandler(this.AppointmentUserControl_Load);
             ((System.ComponentModel.ISupportInitialize)(this.doctorBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.doctorDataGridView)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.BindingSource doctorBindingSource;
-        private System.Windows.Forms.BindingNavigator doctorBindingNavigator;
-        private System.Windows.Forms.ToolStripButton bindingNavigatorAddNewItem;
-        private System.Windows.Forms.ToolStripLabel bindingNavigatorCountItem;
-        private System.Windows.Forms.ToolStripButton bindingNavigatorDeleteItem;
-        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveFirstItem;
-        private System.Windows.Forms.ToolStripButton bindingNavigatorMovePreviousItem;
-        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator;
-        private System.Windows.Forms.ToolStripTextBox bindingNavigatorPositionItem;
-        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator1;
-        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveNextItem;
-        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveLastItem;
-        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator2;
-        private System.Windows.Forms.ToolStripButton doctorBindingNavigatorSaveItem;
         private System.Windows.Forms.DataGridView doctorDataGridView;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
--- ClinicApp/ClinicApp/UserControls/AppointmentUserControl.cs
+++ ClinicApp/ClinicApp/UserControls/AppointmentUserControl.cs
@@ -7,14 +7,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ClinicApp.Controller;
+using ClinicApp.Model;
 
 namespace ClinicApp.UserControls
 {
+    /// <summary>
+    /// User Control to interface with Appointment functionality
+    /// </summary>
     public partial class AppointmentUserControl : UserControl
     {
+        private readonly DoctorController doctorController;
+        private List<Doctor> doctorList;
+        
+        /// <summary>
+        /// Constructor; initializes the User Control and the Controller objects
+        /// </summary>
         public AppointmentUserControl()
         {
             InitializeComponent();
+            this.doctorController = new DoctorController();
+        }
+
+        public void RefreshPage()
+        {
+            this.GetDoctorList();
+        }
+        private void GetDoctorList()
+        {
+            try
+            {
+                doctorList = doctorController.GetDoctorList();
+                doctorDataGridView.DataSource = doctorList;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.GetType().ToString());
+            }
+        }
+
+        private void AppointmentUserControl_Load(object sender, EventArgs e)
+        {
+            this.RefreshPage();
         }
     }
 }
