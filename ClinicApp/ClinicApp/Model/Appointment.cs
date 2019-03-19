96344ed3f5d92f9faacf6a4d17f37f70c991cb89
Jeannie Blakely <jeanbblakely@gmail.com>
2019-03-18 22:53:04 -0400

    Built out models for Appointment

---
+++ ClinicApp/ClinicApp/Model/Appointment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicApp.Model
+{
+    /// <summary>
+    /// Properties with get/set for Appointment attributes
+    /// </summary>
+    public class Appointment
+    {
+        public int AppointmentID { get; set; }
+        public int AppointmentPatientID { get; set; }
+        public int AppointmentDoctorID { get; set; }
+        public string AppointmentDoctorFirstName { get; set; }
+        public string AppointmentDoctorLastName { get; set; }
+        public DateTime AppointmentDateTime { get; set; }
+        public string AppointmentReason { get; set; }
+    }
+}
