c91ccfd8367e2e45e3e0abc9442c263cc7702b5f
Joey Marinelli <jmarine1@my.westga.edu>
2019-03-26 09:54:10 -0400

    Removed GetPatientByID from branch off of master

--- ClinicApp/ClinicApp/DAL/PatientDAL.cs
+++ ClinicApp/ClinicApp/DAL/PatientDAL.cs
@@ -67,67 +67,6 @@ namespace ClinicApp.DAL
         }
 
         /// <summary>
-        /// Gets a single patient from the database by patientID
-        /// </summary>
-        /// <param name="personID">The personID to retrieve</param>
-        /// <returns>A Patient object</returns>
-        public static Patient GetPatientByID(int patientID)
-        {
-            Patient patient = new Patient();
-            string selectStatement = 
-                "SELECT patientID, p.personID, lastName, firstName, birthDate, SSN, gender, " +
-                "streetAddress, city, state, postCode, phoneNumber, username " +
-                "FROM Patient " +
-                "JOIN Person p ON Patient.personID = p.personID " +
-                "WHERE patientID = @patientID";
-            using (SqlConnection connection = ClinicDBConnection.GetConnection())
-            {
-                connection.Open();
-                using (SqlCommand selectCommand = new SqlCommand(selectStatement, connection))
-                {
-                    selectCommand.Parameters.AddWithValue("@patientID", patientID);
-                    using (SqlDataReader reader = selectCommand.ExecuteReader(CommandBehavior.SingleRow))
-                    {
-                        int patientIDOrd = reader.GetOrdinal("patientID");
-                        int personIDOrd = reader.GetOrdinal("personID");
-                        int lastNameOrd = reader.GetOrdinal("lastName");
-                        int firstNameOrd = reader.GetOrdinal("firstName");
-                        int birthDateOrd = reader.GetOrdinal("birthDate");
-                        int ssnOrd = reader.GetOrdinal("SSN");
-                        int genderOrd = reader.GetOrdinal("gender");
-                        int addressOrd = reader.GetOrdinal("streetAddress");
-                        int cityOrd = reader.GetOrdinal("city");
-                        int stateOrd = reader.GetOrdinal("state");
-                        int postCodeOrd = reader.GetOrdinal("postCode");
-                        int phoneOrd = reader.GetOrdinal("phoneNumber");
-                        int usernameOrd = reader.GetOrdinal("username");
-                        if (reader.Read())
-                        {
-                            patient.PatientID = reader.GetInt32(patientIDOrd);
-                            patient.PersonID = reader.GetInt32(personIDOrd);
-                            patient.LastName = reader.GetString(lastNameOrd);
-                            patient.FirstName = reader.GetString(firstNameOrd);
-                            patient.BirthDate = reader.GetDateTime(birthDateOrd);
-                            patient.SSN = reader.GetString(ssnOrd);
-                            patient.Gender = reader.GetString(genderOrd);
-                            patient.StreetAddress = reader.GetString(addressOrd);
-                            patient.City = reader.GetString(cityOrd);
-                            patient.State = reader.GetString(stateOrd);
-                            patient.PostCode = reader.GetString(postCodeOrd);
-                            patient.PhoneNumber = reader.GetString(phoneOrd);
-                            if (!reader.IsDBNull(usernameOrd)) patient.Username = reader.GetString(usernameOrd);
-                        }
-                        else
-                        {
-                            patient = null;
-                        }
-                    }
-                }
-            }
-            return patient;
-        }
-
-        /// <summary>
         /// Gets a patient from the database based on personal details
         /// </summary>
         /// <param name="firstName">Patient's first name</param>
