c7900e8ffd256f4b588e28513ae706cd96984245
Jeannie Blakely <jeanbblakely@gmail.com>
2019-04-06 08:24:59 -0400

    Added OrderedTest UserControl; built out Models, DAL for LabTest; hooked testList to ComboBox

---
+++ ClinicApp/ClinicApp/Controller/LabTestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using ClinicApp.Model;
+using ClinicApp.DAL;
+
+namespace ClinicApp.Controller
+{
+
+    /// <summary>
+    /// Controller for LabTest class
+    /// </summary>
+    public class LabTestController
+    {
+
+        public List<LabTest> GetLabTests()
+        {
+            return LabTestDAL.GetTestList();
+        }
+    }
+}
