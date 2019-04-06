b351718f193e85011f85fc1ce3258e5ee6bd31fe
James Luke Johnson <jjohn144@my.westga.edu>
2019-04-05 22:44:37 -0500

    Created AdministratorDashboard, PersonSearchUserControl, and LoginUserControl. Modified LoginForm to open correct dashboard.

---
+++ ClinicApp/ClinicApp/View/AdministratorDashboard.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows.Forms;
+
+namespace ClinicApp.View
+{
+    public partial class AdministratorDashboard : Form
+    {
+        private static AdministratorDashboard instance = null;
+        public AdministratorDashboard()
+        {
+            InitializeComponent();
+        }
+
+        public static AdministratorDashboard Instance()
+        {
+            if (instance == null)
+            {
+                instance = new AdministratorDashboard();
+            }
+            return instance;
+        }
+
+        private void AdministratorDashboard_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void logoutLinkLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            AdministratorDashboard.Instance().Hide();
+            LoginForm.Instance().Show();
+            LoginForm.ClearFields();
+        }
+    }
+}
