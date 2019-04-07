0e753a714f29e4a70ad7a169eb055b54e88cc141
Joey Marinelli <jmarine1@my.westga.edu>
2019-04-07 13:16:12 -0400

    Added security class to controllers

---
+++ ClinicApp/ClinicApp/Controller/Security.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicApp.Controller
+{
+    public class Security
+    {
+        public static string HashSHA1(string value)
+        {
+            var sha1 = System.Security.Cryptography.SHA1.Create();
+            var inputBytes = Encoding.ASCII.GetBytes(value);
+            var hash = sha1.ComputeHash(inputBytes);
+
+            var builder = new StringBuilder();
+            for (int index = 0; index < hash.Length; index++)
+            {
+                builder.Append(hash[index].ToString("X2"));
+            }
+
+            return builder.ToString();
+        }
+    }
+}
