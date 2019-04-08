9aa3fda983d6a1705b8638ce991df272fb52569c
Joey Marinelli <jmarine1@my.westga.edu>
2019-04-08 00:12:16 -0400

    Added GetNurse and GetAdmin to this branch

--- ClinicApp/ClinicApp/DAL/CredentialDAL.cs
+++ ClinicApp/ClinicApp/DAL/CredentialDAL.cs
@@ -12,6 +12,73 @@ namespace ClinicApp.DAL
    
     class CredentialDAL
     {
+        public static Nurse GetNurseByUserName(string username)
+        {
+            Nurse nurse = new Nurse();
+            string selectStatement = "SELECT p.firstName, p.lastName, c.username, c.password, c.role " +
+                "FROM Credential c " +
+                "JOIN Person p " +
+                "ON p.username = c.username " +
+                "JOIN Nurse n " +
+                "ON n.personID = p.personID " +
+                "WHERE c.username = @username";
+            using (SqlConnection connection = ClinicDBConnection.GetConnection())
+            {
+                connection.Open();
+                using (SqlCommand selectCommand = new SqlCommand(selectStatement, connection))
+                {
+                    selectCommand.Parameters.AddWithValue("@username", username);
+                    using (SqlDataReader reader = selectCommand.ExecuteReader())
+                    {
+                        int fNameOrd = reader.GetOrdinal("firstName");
+                        int lNameOrd = reader.GetOrdinal("lastName");
+                        int uNameOrd = reader.GetOrdinal("username");
+                        int rolOrd = reader.GetOrdinal("role");
+                        if (reader.Read())
+                        {
+                            nurse.FirstName = reader.GetString(fNameOrd);
+                            nurse.LastName = reader.GetString(lNameOrd);
+                            nurse.Username = reader.GetString(uNameOrd);
+                        }
+                    }
+                }
+            }
+            return nurse;
+        }
+
+        public static Administrator GetAdministratorByUserName(string username)
+        {
+            Administrator administrator = new Administrator();
+            string selectStatement = "SELECT p.firstName, p.lastName, c.username, c.password, c.role " +
+                "FROM Credential c " +
+                "JOIN Person p " +
+                "ON p.username = c.username " +
+                "JOIN Administrator a " +
+                "ON a.personID = p.personID " +
+                "WHERE c.username = @username";
+            using (SqlConnection connection = ClinicDBConnection.GetConnection())
+            {
+                connection.Open();
+                using (SqlCommand selectCommand = new SqlCommand(selectStatement, connection))
+                {
+                    selectCommand.Parameters.AddWithValue("@username", username);
+                    using (SqlDataReader reader = selectCommand.ExecuteReader())
+                    {
+                        int fNameOrd = reader.GetOrdinal("firstName");
+                        int lNameOrd = reader.GetOrdinal("lastName");
+                        int uNameOrd = reader.GetOrdinal("username");
+                        int rolOrd = reader.GetOrdinal("role");
+                        if (reader.Read())
+                        {
+                            administrator.FirstName = reader.GetString(fNameOrd);
+                            administrator.LastName = reader.GetString(lNameOrd);
+                            administrator.Username = reader.GetString(uNameOrd);
+                        }
+                    }
+                }
+            }
+            return administrator;
+        }
 
         public static string GetPassword(string username)
         {
