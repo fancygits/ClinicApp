04572d333d5a901c1c7428f281bb428800e7e9f0
James Luke Johnson <jjohn144@my.westga.edu>
2019-04-07 21:57:04 -0500

    Modified credentialTableLayoutPanel in both Dashboards. Added lines, changed colors.

--- ClinicApp/ClinicApp/View/AdministratorDashboard.cs
+++ ClinicApp/ClinicApp/View/AdministratorDashboard.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Forms;
+﻿using System.Drawing;
+using System.Windows.Forms;
 
 namespace ClinicApp.View
 {
@@ -18,7 +19,7 @@ namespace ClinicApp.View
             }
             return instance;
         }
-
+        
         private void AdministratorDashboard_FormClosing(object sender, FormClosingEventArgs e)
         {
             Application.Exit();
@@ -29,6 +30,17 @@ namespace ClinicApp.View
             AdministratorDashboard.Instance().Hide();
             LoginForm.Instance().Show();
             LoginForm.ClearFields();
+        }
+
+        private void DrawLines(object sender, PaintEventArgs e)
+        {
+            Pen pen = new Pen(Color.Gray, 3);            
+            Point start1 = new Point(lblRole.Location.X, lblRole.Location.Y + 10);
+            Point end1 = new Point(lblRole.Location.X, lblRole.Location.Y + 30);
+            Point start2 = new Point(logoutLinkLabel.Location.X, logoutLinkLabel.Location.Y + 10);
+            Point end2 = new Point(logoutLinkLabel.Location.X, logoutLinkLabel.Location.Y + 30);
+            e.Graphics.DrawLine(pen, start1, end1);
+            e.Graphics.DrawLine(pen, start2, end2);
         }
     }
 }
