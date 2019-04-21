9547cf9173f98783de27b9e826d0705d4c78b733
Jeannie Blakely <jeanbblakely@gmail.com>
2019-04-21 16:56:47 -0400

    Some work with sproc and labtestreport

---
+++ ClinicApp/ClinicApp/UserControls/LabTestReportUserControl.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace ClinicApp.UserControls
+{
+    partial class LabTestReportUserControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this._CS6232_g1DataSet = new ClinicApp._CS6232_g1DataSet();
+            this.spmostPerformedTestsDuringDatesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.sp_mostPerformedTestsDuringDatesTableAdapter = new ClinicApp._CS6232_g1DataSetTableAdapters.sp_mostPerformedTestsDuringDatesTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this._CS6232_g1DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.spmostPerformedTestsDuringDatesBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // reportViewer1
+            // 
+            reportDataSource1.Name = "LabReportsPerformedDataSet";
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "ClinicApp.LabTestPerformedReport.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(0, 60);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.ServerReport.BearerToken = null;
+            this.reportViewer1.Size = new System.Drawing.Size(640, 338);
+            this.reportViewer1.TabIndex = 0;
+            // 
+            // _CS6232_g1DataSet
+            // 
+            this._CS6232_g1DataSet.DataSetName = "_CS6232_g1DataSet";
+            this._CS6232_g1DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // spmostPerformedTestsDuringDatesBindingSource
+            // 
+            this.spmostPerformedTestsDuringDatesBindingSource.DataMember = "sp_mostPerformedTestsDuringDates";
+            this.spmostPerformedTestsDuringDatesBindingSource.DataSource = this._CS6232_g1DataSet;
+            // 
+            // sp_mostPerformedTestsDuringDatesTableAdapter
+            // 
+            this.sp_mostPerformedTestsDuringDatesTableAdapter.ClearBeforeFill = true;
+            // 
+            // LabTestReportUserControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.reportViewer1);
+            this.Name = "LabTestReportUserControl";
+            this.Size = new System.Drawing.Size(640, 401);
+            ((System.ComponentModel.ISupportInitialize)(this._CS6232_g1DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.spmostPerformedTestsDuringDatesBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.BindingSource spmostPerformedTestsDuringDatesBindingSource;
+        private _CS6232_g1DataSet _CS6232_g1DataSet;
+        private _CS6232_g1DataSetTableAdapters.sp_mostPerformedTestsDuringDatesTableAdapter sp_mostPerformedTestsDuringDatesTableAdapter;
+    }
+}
---
+++ ClinicApp/ClinicApp/_CS6232_g1DataSet.cs
@@ -0,0 +1,15 @@
+﻿namespace ClinicApp
+{
+
+
+    partial class _CS6232_g1DataSet
+    {
+    }
+}
+
+namespace ClinicApp._CS6232_g1DataSetTableAdapters {
+    
+    
+    public partial class sp_mostPerformedTestsDuringDatesTableAdapter {
+    }
+}
